958b3cb10bd69ddd9cd7eab35feac5db4358f4aa
Xavi <xavi.quesada@iticbcn.cat>
2023-10-10 20:14:20 +0200

    Canvi al text del titol

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter TextField y ListView',
+      title: 'Llista de la compra',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
