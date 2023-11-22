67077dabe920579f85a4ea3826b3cc3d5bddcb16
Xavi <xavi.quesada@iticbcn.cat>
2023-11-22 17:18:06 +0100

    classe

--- lib/models/llista_articles.dart
+++ lib/models/llista_articles.dart
@@ -4,8 +4,7 @@ import "package:flutter/material.dart";
 import 'package:http/http.dart' as http;
 
 class LlistaArticles extends ChangeNotifier {
-  static const serverPath =
-      "http://localhost:5000"; //http://xaviq.pythonanywhere.com";
+  static const serverPath = "http://xaviq.pythonanywhere.com";
   static String apiKey = "";
   Future<List<Article>> fetchArticles() async {
     final response = await http.get(
